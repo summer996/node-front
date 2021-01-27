a45e7965e4fe234d26cc930170014bbbd69e64f2
lichunying <chunying.li@karakal.com.cn>
2021-01-27 17:15:11 +0800

    feat: 🎸 添加路由

--- src/Page.tsx
+++ src/Page.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import Login from "./components/pages/Login";
+import NotFound from './components/pages/NotFound';
+import App from './App';
 
 const Page = () => {
-  return <div>fadfasds</div>
+  return (
+    <HashRouter>
+      <Switch>
+        <Route exact path="/" render={() => <Redirect to="/login" />} />
+        <Route path="/login" component={Login} />
+        <Route path="/app" component={App} />
+        <Route component={NotFound} />
+      </Switch>
+    </HashRouter>
+  );
 }
 
 export default Page;
